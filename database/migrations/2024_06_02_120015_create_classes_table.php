56eee011c7fbc19fa3b4f41bc4fa929527f984be
Nour-Cherrat <cherrat.nour@gmail.com>
2024-06-09 16:48:34 +0100

    add desc to classes

--- database/migrations/2024_06_02_120015_create_classes_table.php
+++ database/migrations/2024_06_02_120015_create_classes_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string('libelle');
             $table->integer('num_salle');
+            $table->string('desc');
             $table->timestamps();
         });
     }
--- database/seeders/ClasseSeeder.php
+++ database/seeders/ClasseSeeder.php
@@ -2,7 +2,6 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 
@@ -16,16 +15,19 @@ class ClasseSeeder extends Seeder
         DB::table('classes')->insert([
             'libelle' => 'Cours 1',
             'num_salle' => '1',
+            'desc' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
         ]);
 
         DB::table('classes')->insert([
             'libelle' => 'Cours 2',
             'num_salle' => '2',
+            'desc' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
         ]);
 
         DB::table('classes')->insert([
             'libelle' => 'Cours 3',
             'num_salle' => '3',
+            'desc' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
         ]);
     }
 }
